362d88be3d9f0e1ab12bdc2adde906278478c902
darkwebplayer <amrithanadhm@gmail.com>
2024-09-10 11:20:11 +0530

    added CSRF Origin

--- Techgraspers/settings.py
+++ Techgraspers/settings.py
@@ -135,3 +135,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://techgraspers.com',
+    'http://techgraspers.com',
+    'https://www.techgraspers.com',
+]
